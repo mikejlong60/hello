c63ac86ea5ba3bc61948fbe58a4fa2409f070842
michael.long <michael.long@deciphernow.com>
2018-11-30 16:28:16 -0500

    Function composition example

--- hello.go
+++ hello.go
@@ -3,8 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/user/stringutil"
+	"github.com/user/fp"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("Hello, world."))
+	fmt.Println(stringutil.Reverse("Hdddello, world."))
+
+	var ff = fp.Compose(f, g)
+	var res = ff("crap")
+	fmt.Println(res)
+}
+
+func g(s string) string {
+	fmt.Println("in g")
+	return s + "fred"
+}
+
+func f(s string) string {
+	fmt.Println("in f")
+	return s + "simon"
 }
